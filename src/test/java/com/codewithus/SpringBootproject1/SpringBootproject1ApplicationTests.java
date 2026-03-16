238771aa86f701f281cf5b5fe37886ba7b864f36
HawiDegefa <hawiha275@gmail.com>
2026-03-17 02:20:31 +0300

    replace project

---
+++ src/test/java/com/codewithus/SpringBootproject1/SpringBootproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithus.SpringBootproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
