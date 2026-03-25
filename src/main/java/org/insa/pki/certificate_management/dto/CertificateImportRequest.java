a6f9dab7bb6e982e1801f29c17c11254cae96cad
Admin <Yirubal117@gmail.com>
2026-03-25 11:09:57 -0700

    phase 2 and phase 3

--- src/main/java/org/insa/pki/certificate_management/dto/CertificateImportRequest.java
+++ src/main/java/org/insa/pki/certificate_management/dto/CertificateImportRequest.java
@@ -4,9 +4,7 @@ public class CertificateImportRequest {
     private String alias;
     private String pem;
 
-
     public CertificateImportRequest() {}
-
 
     public void setAlias(String alias) { this.alias= alias; }
     public void setPem(String pem) { this.pem = pem; }
